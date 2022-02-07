d4318f20b0e49b2fa2fe20b91ad06658dbe4f35f
berkan <berkansems@gmail.com>
2022-02-07 20:22:26 +0300

    setup modified for better README user experience

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
+import os
 from setuptools import setup
 
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
 setup(
     name="latin_to_arabic_number",
-    version="0.0.1",
-    description="This is a library to reverse numbers order and change latin numbers to arabic in a .docx file",
+    version="0.0.3",
+    description="This library helps to reverse numbers order OR change latin numbers to arabic in a .docx file",
     py_modules=["latin_to_arabic_number"],
     package_dir= {'':'src'},
-    long_description= 'Reverse Latin Numbers Order || Change It To Arabic',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
     classifiers=[
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
